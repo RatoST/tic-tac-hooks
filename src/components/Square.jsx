ad052ed7fccb853167fc43a780e2c9d78262ff57
mbonaci <mbonaci@gmail.com>
2020-03-31 22:03:34 +0200

    Fix Square prop-types

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-
 // Square component - each square of the game is button with 2 props X and O
-function Square({value, onClick}) {
-    return(
-      <button className="square" onClick={onClick}>
-        {value}
-      </button>
-    )
+function Square({ value, onClick }) {
+  return(
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  )
 }
 
-Square.PropTypes = {
-    value: PropTypes.string,
-    onClick: PropTypes.func
+Square.propTypes = {
+  value: PropTypes.string,
+  onClick: PropTypes.func
 }
-
 
 export default Square;
