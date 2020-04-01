7cd2010284d99bab04c1c62a52b3d75bd69cf2b0
mbonaci <mbonaci@gmail.com>
2020-04-01 19:26:50 +0200

    App and Header with score

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Game from "./components/Game";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
+  //Keeping score
+  const [scoreA, setScoreA] = useState({ name: 'Game A', countX: 0, countO: 0 });
+  const [scoreB, setScoreB] = useState({ name: 'Game B', countX: 0, countO: 0 });
+
+  function score(winner, score, setScore) {
+    if (winner === "X") {
+        setScore({ ...score, countX: score.countX + 1 });
+      } else if (winner === "O") {
+        setScore({ ...score, countO: score.countO + 1 });
+    }
+  }
+
+  function onScoreA(winner) {
+    score(winner, scoreA, setScoreA);
+  }
+
+  function onScoreB(winner) {
+    score(winner, scoreB, setScoreB);
+  }
 
   return(
   <div>
     <div>
-      <Header />
+      <Header scoreA={scoreA} scoreB={scoreB} />
     </div>
-    <div>
-      <Game />
+    <div className="justify">
+      <Game score={onScoreA} />
+      <Game score={onScoreB} />
     </div>
     <div>
       <Footer />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
+import Score from "./Score";
 
-function Header (){
+function Header ({ scoreA, scoreB }){
   return (
     <header>
-        <h1>-Tic-Tac-Toe-Game-</h1>
+        <h1>Tic-Tac-Toe Game</h1>
+        <div className="justify">
+          <Score score={scoreA}/>
+          <Score score={scoreB}/>
+        </div>
     </header>
   )
 }
 
+Header.propTypes = {
+  scoreA: PropTypes.object,
+  scoreB: PropTypes.object
+}
 export default Header;
